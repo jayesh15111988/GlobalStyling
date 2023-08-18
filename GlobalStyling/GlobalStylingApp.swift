9755ed285bd07595995f6d3ca6bbfb0a26835dde
Jayesh Kawli <j.kawli@gmail.com>
2023-08-18 12:16:45 +0200

    Initial Commit

---
+++ GlobalStyling/GlobalStylingApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlobalStylingApp.swift
+//  GlobalStyling
+//
+//  Created by Jayesh Kawli on 8/18/23.
+//
+
+import SwiftUI
+
+@main
+struct GlobalStylingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
